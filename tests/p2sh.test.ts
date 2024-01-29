9a81a527f20cbf9d5969be1393b789734b7dee4d
aaron67 <aaron67@aaron67.cc>
2024-01-29 13:50:03 +0800

    Update p2sh test case

--- tests/p2sh.test.ts
+++ tests/p2sh.test.ts
@@ -1,29 +1,58 @@
 import { expect, use } from 'chai'
-import { hash160, toByteString, bsv } from 'scrypt-ts'
+import { hash160, toByteString, bsv, findSigs, toHex } from 'scrypt-ts'
 import { P2SH } from '../src/contracts/p2sh'
-import { getDefaultSigner } from './utils/txHelper'
+import { getDefaultSigner, randomPrivateKey } from './utils/txHelper'
 import chaiAsPromised from 'chai-as-promised'
-import { myPublicKey } from './utils/privateKey'
+import { myAddress, myPublicKey } from './utils/privateKey'
 use(chaiAsPromised)
 
 describe('Test SmartContract `P2SH`', () => {
     let instance: P2SH
 
-    const asm = `1 ${myPublicKey.toHex()} 1 OP_CHECKMULTISIG`
+    const [aPrivateKey, aPublicKey, , aAddress] = randomPrivateKey()
+    const [bPrivateKey, bPublicKey, , bAddress] = randomPrivateKey()
+
+    // 2 of 3 multisig
+    const asm = `OP_2 ${myPublicKey.toHex()} ${aPublicKey.toHex()} ${bPublicKey.toHex()} OP_3 OP_CHECKMULTISIG`
     const redeemScript = toByteString(bsv.Script.fromASM(asm).toHex())
     const scriptHash = hash160(redeemScript)
+
+    const signer = getDefaultSigner([aPrivateKey, bPrivateKey])
 
     before(async () => {
         await P2SH.compile()
 
         instance = new P2SH(scriptHash)
-        await instance.connect(getDefaultSigner())
+        await instance.connect(signer)
 
-        await instance.deploy()
+        await instance.deploy(100)
     })
 
     it('should pass the public method unit test successfully.', async () => {
-        const call = async () => await instance.methods.redeem(redeemScript)
-        await expect(call()).not.to.be.rejected
+        const { tx } = await instance.methods.redeem(redeemScript)
+        console.log(tx.id)
+
+        const redeemTx = new bsv.Transaction()
+            .addInputFromPrevTx(tx, 0)
+            .feePerKb(1)
+            .change(myAddress)
+        // sign with private keys of alice and bob
+        const sigRequest = {
+            prevTxId: tx.id,
+            outputIndex: 0,
+            inputIndex: 0,
+            satoshis: tx.outputs[0].satoshis,
+            scriptHex: tx.outputs[0].script.toHex(),
+        }
+        const sigResponses = await signer.getSignatures(redeemTx.toString(), [
+            Object.assign({}, sigRequest, { address: aAddress }),
+            Object.assign({}, sigRequest, { address: bAddress }),
+        ])
+        const sigs = findSigs(sigResponses, [aAddress, bAddress])
+        const unlockingAsm = `OP_0 ${sigs.map((sig) => toHex(sig)).join(' ')}`
+        redeemTx.setInputScript(0, bsv.Script.fromASM(unlockingAsm))
+        console.log(await signer.provider?.sendTransaction(redeemTx))
+
+        expect(redeemTx.verify()).to.be.true
     })
 })
